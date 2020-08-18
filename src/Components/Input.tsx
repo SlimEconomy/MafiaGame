41d21d8e07f5f93797db5454561dbd83a8d3c9bf
slimeconomy <kernezitrone@gmail.com>
2020-08-18 22:28:43 +0200

    added autofocus on the input after adding a role or player

--- src/Components/Input.tsx
+++ src/Components/Input.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Button from "@material-ui/core/Button"
 
 interface Props {
@@ -9,19 +9,21 @@ interface Props {
 
 const Input: React.FC<Props> = ({ add, label, labelColor }) => {
     const [inputValue, setInputValue] = useState("");
+    const inputElement = useRef() as React.MutableRefObject<HTMLInputElement>;
 
     const submitForm = (event: React.FormEvent<HTMLFormElement> | React.MouseEvent): void => {
         event.preventDefault();
         if (!inputValue) return alert("Input field cannot be empty.")
         add(inputValue);
         setInputValue("");
+        if (inputElement && inputElement.current) inputElement.current.focus();
     }
 
     return (
         <div className="input">
             <form onSubmit={submitForm}>
                 <label style={{ backgroundColor: labelColor }}>{label}</label>
-                <input type="text" value={inputValue} onChange={e => setInputValue(e.target.value)} />
+                <input type="text" value={inputValue} onChange={e => setInputValue(e.target.value)} ref={inputElement} />
                 <Button variant="contained" color="primary" onClick={submitForm}>Add</Button>
             </form>
         </div>
