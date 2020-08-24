84834436eba8885009cec34e15137da69b3cf65a
slimeconomy <kernezitrone@gmail.com>
2020-08-24 19:43:27 +0200

    Updates

--- precache-manifest.05ba2b19101f7eb4ff9bf95a951b4428.js
+++ precache-manifest.05ba2b19101f7eb4ff9bf95a951b4428.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "981003348cb7fd638184339aa402f0ca",
+    "revision": "768234997748e0726293e071a4791e23",
     "url": "/MafiaGame/index.html"
   },
   {
-    "revision": "27adc52cdfd259969711",
-    "url": "/MafiaGame/static/css/main.29320aa3.chunk.css"
+    "revision": "1edf620185516648b06a",
+    "url": "/MafiaGame/static/css/main.4299e7ca.chunk.css"
   },
   {
-    "revision": "c59498eefe06f02d3690",
-    "url": "/MafiaGame/static/js/2.f5c987b7.chunk.js"
+    "revision": "b213fa3828e5bd1056c6",
+    "url": "/MafiaGame/static/js/2.3097417a.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/MafiaGame/static/js/2.f5c987b7.chunk.js.LICENSE.txt"
+    "url": "/MafiaGame/static/js/2.3097417a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "27adc52cdfd259969711",
-    "url": "/MafiaGame/static/js/main.9ed0f7ec.chunk.js"
+    "revision": "1edf620185516648b06a",
+    "url": "/MafiaGame/static/js/main.afdc35de.chunk.js"
   },
   {
     "revision": "e2b9c7a6e901b47baa6b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/MafiaGame/precache-manifest.7a0738a227c8fb9b3920e4ddcd605c5d.js"
+  "/MafiaGame/precache-manifest.05ba2b19101f7eb4ff9bf95a951b4428.js"
 );
 
 self.addEventListener('message', (event) => {
